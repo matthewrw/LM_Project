6b8170c7c9c12d58e480165d0ee9c5023e8b1427
Benjamin Draves <benjamin.draves@gmail.com>
2017-12-11 12:31:31 -0500

    update figures code

--- code/figure_code.R
+++ code/figure_code.R
@@ -6,9 +6,12 @@
 
 setwd("~/Desktop/github/LM_Project/data")
 train = data.frame(read.csv("train.csv"))
+test = data.frame(read.csv("test.csv"))
 setwd("~/Desktop/github/LM_Project/report/report_figures")
 
-
+#-------------------------------------------
+#       Figure 1 
+#-------------------------------------------
 library(ggplot2)
 library(dplyr)
 
@@ -33,6 +36,10 @@ pdf("day_bar.pdf")
 p2
 dev.off()
 
+#-------------------------------------------
+#       Figure 2
+#-------------------------------------------
+
 p3<-ggplot(data=train, aes(x = "",y=ISI)) +
   geom_boxplot(fill="blue", alpha = 0.5)+
   theme_minimal()+ labs(y = "ISI",x = "", main = "")+
@@ -41,6 +48,9 @@ pdf("ISI_box.pdf")
 p3
 dev.off()
 
+#-------------------------------------------
+#       Figure 3
+#-------------------------------------------
 
 p4<-ggplot(data=train, aes(x =FFMC,y=sqISI)) +
   geom_point(color="blue",alpha = 0.7)+
@@ -48,6 +58,10 @@ p4<-ggplot(data=train, aes(x =FFMC,y=sqISI)) +
 pdf("FFMC_ISI_scatter.pdf")
 p4
 dev.off()
+
+#-------------------------------------------
+#       Figure 5 
+#-------------------------------------------
 
 #construct regression equation
 f1 = formula(sqISI ~ temp + wind + summer + rainvnorain)
@@ -67,6 +81,9 @@ plot_glmnet(fit2, ylim = c(0,.6))
 plot_glmnet(fit1, ylim = c(0,.6)) 
 dev.off()
 
+#-------------------------------------------
+#       Figure 6 
+#-------------------------------------------
 
 train = data.frame(train %>% group_by(tFFMC) %>% mutate(weight = n()))
 m1 = lm(sqISI ~temp + wind + summer + rainvnorain, data = train)
@@ -94,6 +111,9 @@ pdf("FFMC-QQ.pdf")
 grid.arrange(p5,p6,p7, nrow = 1, ncol = 3)
 dev.off()
 
+#-------------------------------------------
+#       Figure 7 
+#-------------------------------------------
 
 train = data.frame(train %>% group_by(tFFMC) %>% mutate(weight = n()))
 m = lm(lgISI ~summer+ wind + temp+ rainvnorain ,data = train)
@@ -105,6 +125,68 @@ car::avPlots(m)
 dev.off()
 summary(m)
 
+#-------------------------------------------
+#       Figure 8
+#-------------------------------------------
+
+#build final model
+m1 = lm(sqISI ~ summer + wind + temp + rainvnorain 
+        #,data = train[-89,])
+        ,data = train)
 
 
+#1000 boostrap samples for each \beta    
+B <- 1000
+ResidualBootstrapM1 <-t( replicate(B, {
+  #yb <- fitted(m1) + resid(m1)[sample.int(nrow(train[-89,]), replace = TRUE)]
+  yb <- fitted(m1) + resid(m1)[sample.int(nrow(train), replace = TRUE)]
+  boot <- model.matrix(m1)
+  coef(lm(yb ~ boot - 1))
+}))
+
+#look at beta distributions
+hist(ResidualBootstrapM1[,1], xlab = "summer")
+hist(ResidualBootstrapM1[,2], xlab ="wind")
+hist(ResidualBootstrapM1[,3], xlab ="temp")
+hist(ResidualBootstrapM1[,4], xlab ="rainvnorain")
+df = data.frame(ResidualBootstrapM1)
+p8 = ggplot(df, aes(x = bootsummer1)) + geom_histogram(fill = "orangered2", color = "steelblue") + theme_bw() + labs(x = "Summer", y ="",main = "")
+p9 = ggplot(df, aes(x = bootwind)) + geom_histogram(fill = "orangered2", color = "steelblue") + theme_bw() + labs(x = "Wind", y ="")
+p10 = ggplot(df, aes(x = boottemp)) + geom_histogram(fill = "orangered2", color = "steelblue") + theme_bw() + labs(x = "Temperature", y ="")
+p11 = ggplot(df, aes(x = bootrainvnorain1)) + geom_histogram(fill = "orangered2", color = "steelblue") + theme_bw() + labs(x = "Rain", y ="")
+
+pdf("boot.pdf")
+grid.arrange(p8,p9,p10,p11, nrow = 2, ncol = 2)
+dev.off()
+
+#empirical CI 
+t(apply(ResidualBootstrapM1, 2, quantile, c(.025, .975)))
+
+
+#-------------------------------------------
+#       Figure 9
+#-------------------------------------------
+
+#transform ISI 
+test$sqISI = sqrt(test$ISI)
+
+#cast as factors 
+test$summer = as.factor(test$summer)
+test$rainvnorain = as.factor(test$rainvnorain)
+
+#final_model
+test_mod = lm(sqISI ~ summer + wind + temp + rainvnorain ,data = test)
+
+plotdf <- data.frame(cbind(test$sqISI^2,test_mod$fitted.values^2) )
+names(plotdf) <-c("ActualValues","FittedValues")
+p12 = ggplot(plotdf, aes(x=FittedValues,y=ActualValues)) + geom_point(col = "steelblue", alpha = .7)+ 
+     geom_segment(aes(x=0,y=0,xend=15,yend=15),col="black") + theme_bw()
+
+pdf("pred_plot.pdf")
+p12
+dev.off()
+
+pdf("TestSetDiagnostics.pdf")
+par(mfrow = c(2,2));plot(test_mod)
+dev.off()
 
